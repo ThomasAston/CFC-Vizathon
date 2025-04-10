67a7a5e833627bfcf2040dd61b68413c7bc8dc0f
ThomasAston <tjn.aston@gmail.com>
2025-04-10 17:32:57 +0100

    updating physical_development

---
+++ pages/physical_development.py
@@ -0,0 +1,11 @@
+from utils.components import date_slider, collapsible_section
+
+# def render_physical_development(player_id):
+#     return date_slider(
+#         label_id="reporting-period-label",
+#         slider_id="reporting-slider",
+#         min_date=min_date,
+#         max_date=max_date,
+#         initial_weeks=6,
+#         output_id="load-demand-output"
+#     )
